2af2bd0ce200406a0e4f31242b3454f52b90e7ca
Diggo 🤙🗿🤙 <125403873+RgoSL@users.noreply.github.com>
2024-05-23 14:40:30 -0300

    pessoaController.php

--- ProjetoWeb/controller/pessoaController.php
+++ ProjetoWeb/controller/pessoaController.php
@@ -1,19 +1,18 @@
 <?php
 
-require_once '../model/pessoas.php'; // Requerindo o arquivo que contém a definição da classe Pessoa. O 'require_once' garante que o arquivo seja incluído apenas uma vez.
+require_once '../model/pessoas.php'; // Instanciando a Classe Pessoa na classe controller. O 'require_once' garante a prioridade dessa linha.//
 
 class PessoaController{
-    private $pessoa; // Declaração de uma propriedade privada $pessoa, que será uma instância da classe Pessoa.
+    private $pessoa; // Declaração de uma propriedade privada $pessoa, que será uma instância da classe Pessoa.//
 
-    public function __construct(){ // Método construtor da classe PessoaController.
-        $this->pessoa = new Pessoa(); // Instanciando um objeto da classe Pessoa e atribuindo-o à propriedade $pessoa.
-        $this->inserir(); // Chamando o método inserir ao construir um objeto PessoaController.
+    public function __construct(){ // Método construtor da classe PessoaController.//
+        $this->pessoa = new Pessoa(); // Instanciando um objeto da classe Pessoa e atribuindo-o à propriedade $pessoa.//
+        $this->inserir(); // Chamando o método inserir ao construir um objeto PessoaController.//
     }
 
-    public function inserir(){ // Método inserir para inserir uma nova pessoa no banco de dados.
-        // Definindo os atributos da pessoa com base nos dados recebidos via POST.
+    public function inserir(){  //Método Inserir do código, ele fará a atribuição dos dados nos campos HTML;
         $this->pessoa->setNome($_POST['nome']);
-        $this->pessoa->setEndereco($_POST['endereco']);
+        $this->pessoa->setEndereco($_POST['endereco']);   // Definindo os atributos da pessoa com base nos dados recebidos via POST.//
         $this->pessoa->setBairro($_POST['bairro']);
         $this->pessoa->setCep($_POST['cep']);
         $this->pessoa->setCidade($_POST['cidade']);
@@ -21,12 +20,12 @@ class PessoaController{
         $this->pessoa->setTelefone($_POST['telefone']);
         $this->pessoa->setCelular($_POST['celular']);
 
-        // Chamando o método inserir da instância da classe Pessoa para inserir os dados no banco de dados.
+        // Chamando o método inserir da instância da classe Pessoa para inserir os dados no Banco de Dados.//
         $this->pessoa->inserir();
 
     }
 }
 
-new PessoaController(); // Criando uma nova instância da classe PessoaController.
+new PessoaController(); // Criando uma nova instância da classe PessoaController.//
 
 ?>
